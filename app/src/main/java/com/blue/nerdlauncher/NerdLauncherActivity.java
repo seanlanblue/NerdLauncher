f3b87451affaa2ee5d77ead6855fef43a631700a
Jun Lan <blue@Juns-MBP.telecom>
2015-01-16 09:15:33 +1300

    Initial commit

---
+++ app/src/main/java/com/blue/nerdlauncher/NerdLauncherActivity.java
@@ -0,0 +1,12 @@
+package com.blue.nerdlauncher;
+
+
+import android.support.v4.app.Fragment;
+
+public class NerdLauncherActivity extends SingleFragmentActivity {
+
+    @Override
+    public Fragment createFragment() {
+        return new NerdLauncherFragment();
+    }
+}
---
+++ app/src/main/java/com/blue/nerdlauncher/SingleFragmentActivity.java
@@ -0,0 +1,34 @@
+package com.blue.nerdlauncher;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+/**
+ * Created by blue on 1/13/15.
+ */
+public abstract class SingleFragmentActivity extends FragmentActivity {
+    protected abstract Fragment createFragment();
+
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_fragment);
+        setContentView(getLayoutResId());
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if(fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
+    }
+
+}
